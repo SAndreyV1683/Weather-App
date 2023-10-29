9efb4c1bcdf1371d4e1cb06a63415b4e104aa0c6
Andrey <a.sboev@teleswyz.ru>
2023-10-29 19:55:41 +0300

    main screen

---
+++ app/src/main/java/a/sboev/weatherappcompose/screens/MainScreen.kt
@@ -0,0 +1,74 @@
+package a.sboev.weatherappcompose.screens
+
+import a.sboev.weatherappcompose.R
+import a.sboev.weatherappcompose.ui.theme.BlueLight
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
+
+@Preview(showBackground = true)
+@Composable
+fun MainScreen() {
+    Image(
+        painter = painterResource(id = R.drawable.weather_backgroud),
+        contentDescription = "image",
+        modifier = Modifier
+            .fillMaxSize()
+            .alpha(0.5f),
+        contentScale = ContentScale.FillBounds
+    )
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(5.dp)
+    ) {
+        Card(
+            elevation = CardDefaults.cardElevation(0.dp),
+            modifier = Modifier.fillMaxWidth(),
+            colors = CardDefaults.cardColors(BlueLight),
+            shape = RoundedCornerShape(10.dp)
+        ) {
+            Column (
+                modifier = Modifier.fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Row (
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    Text(
+                        modifier = Modifier.padding(top = 8.dp, start = 8.dp),
+                        text = "20 Jun 2023",
+                        style = TextStyle(fontSize = 15.sp),
+                        color = Color.White
+                    )
+                    AsyncImage(
+                        modifier = Modifier.size(35.dp).padding(top = 3.dp, end = 8.dp),
+                        model = "https://cdn.weatherapi.com/weather/64x64/night/368.png",
+                        contentDescription = "im2")
+                }
+            }
+        }
+    }
+}
